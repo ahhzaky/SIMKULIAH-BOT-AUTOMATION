840ad6d95bd5029810261051dc485703fc8b3edd
ahhzaky <siahmadzaky@gmail.com>
2020-12-17 14:41:01 +0700

    :gear: click absen

--- bot_sim.py
+++ bot_sim.py
@@ -3,14 +3,12 @@ import time
 import json
 from bs4 import BeautifulSoup
 from selenium import webdriver
-from dataclasses import dataclass
 from selenium.webdriver.common.keys import Keys
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.common.exceptions import NoSuchElementException, ElementClickInterceptedException
 
+
 # baca file json
-
-
 def read_credentials():
     secrets = 'secrets.json'
     with open(secrets) as f:
@@ -19,6 +17,7 @@ def read_credentials():
 
 
 class SIMkuliah:
+    # prepare data
     def __init__(self, login, password):
         self.login = login
         self.password = password
@@ -27,16 +26,19 @@ class SIMkuliah:
         self.driver = webdriver.Chrome(ChromeDriverManager().install())
         self.COMMAND_OR_CONTROL = Keys.COMMAND if sys.platform == 'darwin' else Keys.CONTROL
 
+    # session 1
     def single_interation_over_session(self):
         self.login_to_simkuliah()
         time.sleep(5)
         self.driver.quit()
 
+    # login and absen
     def login_to_simkuliah(self):
         self.driver.get(self.base_url)
         self.put_credentials_to_form()
         self.get_to_absen_simkuliah()
 
+    # input data
     def put_credentials_to_form(self):
         try:
             # Enter user credentials and Click on Submit button to Logins
@@ -50,13 +52,18 @@ class SIMkuliah:
         except NoSuchElementException:
             print("Exception NoSuchElementException")
 
+    # click absen
     def get_to_absen_simkuliah(self):
         try:
-            self.driver.find_element_by_link_text('Python').click()
+            self.driver.find_element_by_link_text(
+                'Konfirmasi Kehadiran').click()
+            time.sleep(1)
+            self.driver.find_element_by_link_text(
+                'Konfirmasi').click()
             time.sleep(2)
+            print("Anda sudah Absen")
         except NoSuchElementException:
             print("Absen Belum ada")
-        pass
 
 
 if __name__ == '__main__':
